2a7d463920e10b707aab3f40f54a68c53582560b
Anthony RDutra <97138694+AnthonyRdutra@users.noreply.github.com>
2025-04-29 16:01:00 -0300

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,6 +1,14 @@
 const { contextBridge, ipcRenderer } = require("electron");
 
+// Exposes specific APIs from the main process to the renderer process in a secure way
 contextBridge.exposeInMainWorld("electronAPI", {
-  getClientData: (scriptName, inputData = null) => ipcRenderer.invoke("get-client-data", scriptName, inputData),
-  logMessage: (message) => ipcRenderer.send("log-message", message)
+  
+  // Exposes a method to request data from a PHP script through IPC
+  // It uses ipcRenderer.invoke to call "get-client-data" and optionally sends input data
+  getClientData: (scriptName, inputData = null) => 
+    ipcRenderer.invoke("get-client-data", scriptName, inputData),
+
+  // Exposes a method to send log messages to the main process
+  logMessage: (message) => 
+    ipcRenderer.send("log-message", message)
 });
